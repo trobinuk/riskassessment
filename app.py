3453e54d501afd726cd27d428911705b0ccdca2e
Robinson Tamilselvan <100284379+trobinuk@users.noreply.github.com>
2023-02-21 18:46:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -365,6 +365,8 @@ def apiTesting():
             print('Error2: '+url)
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    #port = int(os.environ.get('PORT', 5000))
+    #app.run(host='0.0.0.0', port=port)
     #app.run(debug=False)#,port=2000
+    app.run()
+    
