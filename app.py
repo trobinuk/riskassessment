5cba4ecb25f1ad1d9a6394b3d589a92b40b7172a
Robinson Tamilselvan <100284379+trobinuk@users.noreply.github.com>
2023-05-19 11:38:55 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,28 +1,30 @@
+#Flask app (API) begins
 import os
 from flask import Flask,jsonify,request
 
 app = Flask(__name__)
 
-@app.route('/')#,methods=["GET"])
-
+@app.route('/',methods=["GET"])
 #app.route("/result",methods=["POST","GET"])
 
+#Flask app (API) Ends
+
 def apiTesting():
-    #return {"API":"Response Positive and negative"} #http://127.0.0.1:2000
-    #return "Hi Robinson!!"
-    #def google_results():
-    #query = request.args['searchName']
-    query = "Adela Williams"
+
+    #importing the neccessary libraries
+    query = request.args['searchName']
     from googlesearch import search #pip install google
     import requests
     from bs4 import BeautifulSoup #pip install bs4
-    import spacy
-    nlp = spacy.load("en_core_web_sm")
+    import spacy 
+    nlp = spacy.load("en_core_web_sm") #python -m spacy download en_core_web_sm
     import pandas as pd 
     from openpyxl import load_workbook
     import re
     from spacy.matcher import Matcher
-
+    import json
+    
+    #Function to clean the text like make spaces etc
     def align_sentence(sent):
         
         sent_split = sent.split()
@@ -41,7 +43,8 @@ def apiTesting():
                 result_sent_list.append(y)
             sent_result = ' '.join(result_sent_list)
         return sent_result
-
+    
+    #Function to extract date of birth
     def dob_pattern(text):
         
         doc = nlp(text)
@@ -77,9 +80,10 @@ def apiTesting():
                     string_id = nlp.vocab.strings[match_id]
                     span = doc[start_pos:end_pos]
                     list_return = [string_id,span.text,start_pos,end_pos]
-
+    
         return list_return
-
+    
+    #Function to extract address
     def address_pattern(text):
         
         doc = nlp(text)
@@ -105,27 +109,32 @@ def apiTesting():
             list_return = [string_id,span.text,start_pos,end_pos]  
 
         return list_return
-
+    
+    #Function to extract social media accounts like Instagram, Facebook, LinkedIN, Twitter
     def find_social_media_acnt(inp_list,line):
         id_name_type = []
+        dictSocialMedia = {"https://www.instagram.com":"Instagram ID",".linkedin.com":"LinkedIn Profile",
+                           "https://www.facebook.com":"Facebook ID","twitter.com":"Twitter ID"}
         for soc_med_type in inp_list:
             li = list(line.split(' '))
-            #print(li)
             for idx,i in enumerate(li):
                 if soc_med_type in i and soc_med_type != 'https://www.facebook.com': 
-                    id_name_type.append([li[idx+2],soc_med_type])
+                    #print('li[idx+2] ',li[idx+2],'  soc_med_type  ',soc_med_type)
+                    id_name_type.append([li[idx+2],dictSocialMedia[soc_med_type]])
                     break
                 if soc_med_type in i and soc_med_type == 'https://www.facebook.com':
                     if li[idx+2] in ('public','people'):
                         if str(li[idx+4])[-1]:
-                            id_name_type.append([li[idx+4]+li[idx+5],soc_med_type])
+                            id_name_type.append([li[idx+4]+li[idx+5],dictSocialMedia[soc_med_type]])
                         else:
-                            id_name_type.append([li[idx+4],soc_med_type])
+                            id_name_type.append([li[idx+4],dictSocialMedia[soc_med_type]])
                         break
-                    id_name_type.append([li[idx+2],soc_med_type])
+                    id_name_type.append([li[idx+2],dictSocialMedia[soc_med_type]])
         return id_name_type
         #Match found and then call the append_list function
-        
+    
+    #Function to append the matches found to the resulting list   
+    
     def append_list(inp_list):
         output_match_term.append(inp_list[0])
         output_word.append(inp_list[1])
@@ -143,14 +152,15 @@ def apiTesting():
 
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36'}
     mod_name = 'Default'
-
+    
+    #patterns for finding the phone number
     phone_pattern_1 = '[\d]?[(]{1}[\d]{3}[)]{1}[\s]?[\d]{3}[-\s]{1}[\d]{4}'
     phone_pattern_2 = '[+]{1}[\d]{2}[\s]{1}[(]?[\d]?[)]?[\d]{2}[\s]{1}[\d]{4}[\s]{1}[\d]{4}'
     phone_pattern_3 = '[+]{1}[\d]{2}[\s]{1}[(]?[\d]?[)]?[\d]{3}[\s]{1}[\d]{3}[\s]{1}[\d]{3}'
     phone_pattern_4 = '[\d]{3}[-\s]{1}[\d]{3}[-\s]{1}[\d]{4}'
     phone_pattern = [phone_pattern_1,phone_pattern_2,phone_pattern_3,phone_pattern_4]
 
-    #for query in search_list:
+
     cnt = 1
     google_result_link = []
     cnt_list = []
@@ -176,29 +186,30 @@ def apiTesting():
     line_no_list = []
     text_in_the_line = []
     url_no = []
-
-    for url in search(query, tld="com", num=11, stop=11, pause=2):
+    
+    #code to do the web scraping
+    for url in search(query, tld="com", num=21, stop=21, pause=2):
         #print(str(cnt)+' '+url)
         try:        
-            if url[-4:] == '.pdf' or url.find('-image?') != -1:
+            if url[-4:] == '.pdf' or url.find('-image?') != -1: #If its a pdf or image file it skips and go the next search result
                 raise ValueError('It is a pdf or image file')    
             google_result_link.append(url)
             cnt_list.append(cnt)
-            if cnt == 11:
+            if cnt == 21: #If cnt == 21 then the first google page link will be downloaded and data will be extracted
                 url = name = query.replace(' ','+')
                 url = 'https://www.google.com/search?q='+name
 
             page = requests.get(url,headers=headers)
             soup = BeautifulSoup(page.text,'html.parser')
             parsed_text = soup.get_text()
-            text = parsed_text.split('\n')
-            text = list(filter(lambda x: x not in ('','\r'),text))    
+            text = parsed_text.split('\n') # Cleaning the data 
+            text = list(filter(lambda x: x not in ('','\r'),text))   # Cleaning the data 
             
             data_into_list = []
             try:  
                 for line in text:
                     if not line.isspace():
-                        if cnt == 11:
+                        if cnt == 21:
                             if line.find('https://') > -1:
                                 line = line.replace('https://',' https://')
                         aligned_line = align_sentence(line.strip())
@@ -206,8 +217,13 @@ def apiTesting():
                         for i in splitted_lines_list:
                             data_into_list.append(i)
                 line_no = 0
+                
+                # All the scraped data are appended to the "data_into_list" varaible
+                # from that list looping through the records and find out the PII by calling each function
+                
                 for line in data_into_list:
                     
+                    # Searching for email matches
                     email_matches = re.finditer('\S+@\S+', line)
                     for m in email_matches:
                         match_count = 0  
@@ -219,7 +235,8 @@ def apiTesting():
                             match_count = 0 
                         inp_list = ['Email',m.group(0),line,m.start(),m.end(),query,cnt,line_no,match_count]
                         append_list(inp_list) 
-                                        
+                    
+                    # Searching for phone matches                    
                     txt_before_and_after = ''
                     for pattern in phone_pattern:
                         for m in re.finditer(pattern,line):
@@ -237,7 +254,8 @@ def apiTesting():
                                 match_count = 0
                             inp_list = ['Phone',m.group(0),line,m.start(),m.end(),query,cnt,line_no,match_count]
                             append_list(inp_list) 
-
+                    
+                    # Searching for Date_of_Birth matches 
                     pattern_values = dob_pattern(line)  
                     if pattern_values:
                         match_count = 0
@@ -256,7 +274,8 @@ def apiTesting():
                             match_count = 0
                         inp_list = [pattern_values[0],pattern_values[1],line,pattern_values[2],pattern_values[3],query,cnt,line_no,match_count]
                         append_list(inp_list) 
-
+                    
+                    # Searching for Address matches 
                     pattern_values = address_pattern(line)  
                     if pattern_values:
                         match_count = 0
@@ -276,7 +295,8 @@ def apiTesting():
                         inp_list = [pattern_values[0],pattern_values[1],line,pattern_values[2],pattern_values[3],query,cnt,line_no,match_count]
                         append_list(inp_list) 
                     
-                    if cnt == 11:
+                    # Searching for Social media matches only for the 21st link
+                    if cnt == 21:
                         social_media_list = []
                         if line.find('.linkedin.com') > -1:
                             social_media_list.append('.linkedin.com')
@@ -299,9 +319,10 @@ def apiTesting():
                                 match_count = 0 
                             inp_list = [out_social_med_list[0][1],out_social_med_list[0][0],line,'','',query,cnt,line_no,match_count]
                             append_list(inp_list)                          
-
+                    
+                    # Searching for Locations 
                     doc = nlp(str(line.strip()))  
-
+        
                     mod_name = 'Spacy'
                     for ent in doc.ents:
                         if ent.label_ == 'GPE':
@@ -339,34 +360,40 @@ def apiTesting():
         df_person_gpe_count = df_person_gpe_unique.groupby(['Entities'])['Entities'].count().reset_index(name='count').sort_values(by='count',ascending=False)
         df_person_gpe_max = df_person_gpe_count.query('count == count.max()')
 
-        #output = {'Location':df_person_gpe_max.iloc[0,0]}
-
-        return jsonify({'Location':df_person_gpe_max.iloc[0,0]})
-
-        '''inp_list = ['Location',df_person_gpe_max.iloc[0,0],'','','',query,'','',df_person_gpe_max.iloc[0,1]]
+        inp_list = ['Location',df_person_gpe_max.iloc[0,0],'','','',query,'','',df_person_gpe_max.iloc[0,1]]
         append_list(inp_list) 
+        
 
         df = pd.DataFrame({'Match term':output_match_term,'Match Word/Text':output_word,'Line':output_line,
                             'Span start':output_list_span_start,'Span end':output_list_span_end,
                             'output_list_search':output_list_search,'output_list_url_no':output_list_url_no,
                             'output_list_line_no':output_list_line_no,'Match Count':output_list_match_count})
+        
 
-        path = f'{query}_rules_output.xlsx'
-        with pd.ExcelWriter(path,engine='xlsxwriter') as writer1:
-            df.to_excel(writer1, sheet_name=f'{query}',index=False)
+        rslt_df = df[df['Match Count'] > 0]
+        rslt_df = rslt_df.sort_values(['Match Count'], ascending=[0])
+        rslt_df = rslt_df.drop_duplicates( subset = ['Match term', 'Match Word/Text'],  keep = 'last').reset_index(drop = True)
+        rslt_df = rslt_df[['Match term', 'Match Word/Text']]
 
-        with pd.ExcelWriter(path,mode='a',engine='openpyxl',if_sheet_exists='replace') as writer:
-            df_person_gpe_count.to_excel(writer, sheet_name='GPE_Count',index=False) 
+        dictionary = {}
 
-        df1 = pd.DataFrame({'Link_no':cnt_list,'Web Link':google_result_link})
-        with pd.ExcelWriter(path,mode='a',engine='openpyxl',if_sheet_exists='replace') as writer:
-            df1.to_excel(writer, sheet_name='Web_Links_Google_Search',index=False) '''
+        for index,row in rslt_df.iterrows():
+            #print(row['Match term'],' ',row['Match Word/Text'])
+            if row['Match term'] in dictionary:
+                dictionary[row['Match term']].append(row['Match Word/Text'])
+            else:
+                dictionary[row['Match term']] = [row['Match Word/Text']]
+        #print('dictionary ',dictionary)
+        
+        json_object = json.dumps(dictionary, indent = 2) 
+
+        return json_object
+
+
     except Exception as e:
             print('Error2: '+url)
 
 if __name__ == "__main__":
-    #port = int(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port)
-    #app.run(debug=False)#,port=2000
-    app.run()
-    
+    # port = int(os.environ.get('PORT', 5000))
+    # app.run(host='0.0.0.0', port=port)
+    app.run(debug=False)#,port=2000
